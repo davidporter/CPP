1b13636fe02096cc5ba4e8d334fa645535accdc8
David Porter <davidbrianporter@gmail.com>
2017-11-30 11:14:11 -0500

    working with auto, templates and input with cin

--- hi.c
+++ hi.c
@@ -1,8 +1,20 @@
 #include <iostream>
 #include <vector>
 
+template <typename T>
+T dbl(T val)
+{
+    return val+val;
+}
+
 int main()
 {
+    //template <typename U>
+    auto numin = 0.0;
+    double numbin;
     std::vector<int> myVect;
     std::cout << "Hi David" << std::endl;
+    std::cout << "Tell me a number: "; std::cin >> numbin;
+    // std::cin >> numbin;
+    std::cout << "doubles to: " << dbl(numbin) << std::endl;
 }
