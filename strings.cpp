5f1051150c428432c3e6dfb7507aee3ee7843d9c
Porter <dporte25@kent.edu>
2018-01-12 18:39:15 -0500

    stringClassObj += charArray[] works!

--- strings.cpp
+++ strings.cpp
@@ -19,11 +19,14 @@ int main()
     cout << "Howdy! I'm " << name2;
     cout << "What's your name?\n";
     cin >> name1;
+    stringy += name1;
     cout << "Well, " << myString << ", your name has ";
     cout << strlen(name1) << " letters and is stored in an array of " << sizeof(name1) << " bytes.\n";
     cout << "Your first initial is " << name1[0] << ".\n";
     name2[3] = '\0';
     cout << "Here are the first 3 characters of my name: ";
     cout << name2 << endl;
+    cout << "stringy = \"David\" " << std::endl;
+    cout << "stringy += name1 " << stringy<<"\n"; 
     return 0;
 }
