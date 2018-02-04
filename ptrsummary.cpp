008a5cd6f8feffcc2b433b06b1611dd681860fd8
Porter <dporte25@kent.edu>
2018-02-04 15:45:39 -0500

    more pointers!

---
+++ ptrsummary.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() 
+{
+	// declaring pointers
+	int* pi;
+	float* pf;
+
+	// assigning values (addreses) to pointers
+	int someInt = 9;
+	pi = &someInt;
+
+	pf = new flaot;
+
+	std::cout << pi << std::endl;
+	std::cout << "pf " << pf << std::endl;
+
+	return 0;
+}
