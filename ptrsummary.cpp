c4fdce47b057d07910a35c6e02826b67f597eb24
David Porter <davidbrianporter@gmail.com>
2018-02-10 20:23:30 -0500

    a.exe holding up process

--- ptrsummary.cpp
+++ ptrsummary.cpp
@@ -13,7 +13,9 @@ int main()
 	pf = new float;
 
 	std::cout << pi << std::endl;
+	std::cout << "value of pi " << pi[0] << std::endl;
 	std::cout << "pf " << pf << std::endl;
+	std::cout << "value of pf that was never set " << *pf << std::endl;
 
 	return 0;
 }
