b8ff71ec4d73d8d3c965c76f0199197c40efa78d
David Porter <davidbrianporter@gmail.com>
2018-02-15 16:20:00 -0500

    added multiple ways to dereference pointers

--- arrowpointer.cpp
+++ arrowpointer.cpp
@@ -12,24 +12,36 @@ int main()
 
     class Entity{
         public:
+        void Print(int n) const { std::cout << "Hi David! "<< n << std::endl; }
         void Print() const { std::cout << "Hi David! " << std::endl; }
     };
 
     note c;
-    note* d;
+    note* ptr_c = &c;
     Entity e;
-    //Entity* ptr_f;
-
+    Entity* ptr_e = &e;
 
     c.duration = 0.25;
     c.note_name = 'c';
     c.intensity = "forte";
 
-    // d->duration = 0.75;
-
     std::cout << c.intensity << std::endl;
 
+    // derefrence note pointer 
+    note& dref_ptr_c = *ptr_c;
+    std::cout << dref_ptr_c.intensity << std::endl;
+
+    // derefrences note pointer - with ->
+    ptr_c->intensity = "Mezzo";
+    std::cout << ptr_c->intensity << std::endl;   
+
+    // dereference pointer
+    (*ptr_c).intensity = "piano";
+    std::cout << (*ptr_c).intensity << std::endl;    
+
+
+
     std::cout << "Now for the entity: " << std::endl;
-    e.Print();
-    // ptr_f->Print();                                                             // arrow operator allow access to entitypointer
+    e.Print(37);
+    ptr_e->Print(38);    // arrow operator allow access to method in class 
 }
