57e90077168846ea1d3e852f92b021684aac9966
David Porter <davidbrianporter@gmail.com>
2018-02-15 11:43:15 -0500

    *(p3+1) prints 2.001, but *p3 or *(p3) does not!?

--- arraynew.cpp
+++ arraynew.cpp
@@ -3,16 +3,33 @@
 int main()
 {
 	using namespace std;
-	double * p3 = new double [3];
-	p3[0] = 0.2;
-	p3[1] = 0.5;
-	p3[3] = 0.8;
+	// double * p3 = new double [3]; 				// what am I doing here?
+	// p3[0] = 0.2;
+	// p3[1] = 0.5;
+	// p3[3] = 0.8;
+
+	double p3[3] = {2.0, 2.001, 2.002};
+
+	double* ptrp3 = p3;
+
+	cout << *p3 << endl;
+	cout << *(p3+1) << endl;						// shows .001
+	cout << *(p3) << endl;
+
 	cout << "p3[1] is " << p3[1] << ".\n";
-	p3 = p3 + 1;
+	ptrp3 = p3 + 1;
 	cout << "Now p3[0] is " << p3[0] << " and ";
 	cout << "p3[1] is " << p3[1] << "\n";
-	p3 = p3 - 1;
-	delete [] p3;
+	ptrp3 = p3 - 1;
+	delete [] ptrp3;									// deleting 
+
+	cout << endl;
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "Printing p3["<<i<<"] " << p3[i] << endl; 
+	}
+	
 	return 0;
 }
 
