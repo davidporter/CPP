c311661dc9c486c103e3ef6e15048879f95d5ffe
David Porter <davidbrianporter@gmail.com>
2018-02-15 21:15:49 -0500

    revisiting recursion

--- recur.cpp
+++ recur.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 void countdown(int n);
+void liftoff(int);
 
 int main()
 {
 	countdown(4);
+	liftoff(100);
 	return 0;
 }
 
@@ -17,3 +19,13 @@ void countdown(int n)
 	}
 	std::cout << ": Kaboom!\n";
 }
+
+void liftoff(int m)
+{
+	if (m > 0)
+	{ 
+		std::cout << m << " "; 
+		liftoff(m -1);
+	}
+}
+
