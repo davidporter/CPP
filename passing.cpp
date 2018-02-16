e28e5f956dd51ef9ee3805e5290c230d7abb3782
David Porter <davidbrianporter@gmail.com>
2018-02-15 21:06:12 -0500

    working on passing by val and ref with pointers

---
+++ passing.cpp
@@ -0,0 +1,33 @@
+// I'm trying to pass in a pointer so that it changes the
+// value at that address in the method
+#include <iostream>
+
+int byref(int*);
+int byval(int);
+
+auto var = 7;
+auto var2 = 11;
+
+int main()
+{
+	int* ptr = &var;
+
+	std::cout << var << std::endl;
+	std::cout << ptr << std::endl;
+
+	byref(ptr);
+	std::cout << var <<std::endl;
+
+	byval(var2);
+	std::cout << var2 << std::endl;
+}
+
+int byref(int* ptr_int) {
+	*ptr_int = 9;
+	return 0;
+}
+
+int byval(int n) {
+	n = 99;
+	return 0;
+}
