b486f49e50ed7dadba1b08ba6498a0fc07236a0c
David Porter <davidbrianporter@gmail.com>
2018-02-20 10:53:32 -0500

    pointers to multidimensional array - needs elaboration

--- arrptr2d.cpp
+++ arrptr2d.cpp
@@ -5,7 +5,7 @@ void showdata(int ar2[][4], int);
 
 int main()
 {
-    int data[3][4] = {{1,2,3,4}, {9,8,7,6}, {2,4,6,8}};
+    int data[3][4] = {{1,2,3,4}, {9,898,7,6}, {2,4,6,88}};
 
     for (int i = 0; i < 3; i++)
     {
@@ -23,5 +23,11 @@ int main()
 void showdata(int ar2[][4], int size)
 {
     std::cout << "the argument 'ar2[][4]' used in the function is a pointer to the data array \n";
-
+    std::cout << "ar2 = \t\t" << ar2 << std::endl;
+    std::cout << "*ar2 = \t\t" << *ar2 << std::endl;
+    std::cout << "ar2+1 = \t\t" << ar2+1 << std::endl;
+    std::cout << "*(ar2+1) = \t\t" << *(ar2+1) << std::endl;
+    std::cout << "*(ar2+1)+1 = \t\t" << *(ar2+1)+1 << std::endl;
+    std::cout << "*(*(ar2+1)+1) = data[1][1]\t\t" << *(*(ar2+1)+1) << std::endl;
+    
 };
