f448a7bca4a1e0a0e182ae949d1dd7d5f341e67a
David Porter <davidbrianporter@gmail.com>
2018-02-22 14:25:41 -0500

    added functions to gain better understanding

--- arrobj.cpp
+++ arrobj.cpp
@@ -6,15 +6,24 @@
 const int Seasons = 4;
 // template < class T, size_t N > class array; http://www.cplusplus.com/reference/array/array/
 const std::array<std::string, Seasons> Snames = {"Spring", "Summer", "Fall", "Winter"};
+const std::array<std::string, 3> triune = {"father", "son", "ghost"};
 
 void fill(std::array<double, Seasons> * pa);
 void show(std::array<double, Seasons> da);
+
+void fillman(std::array<double, 3>* pb);
+void showman(std::array<double, 3> db);
 
 int main()
 {
     std::array<double, Seasons> expenses;
     fill(&expenses);
     show(expenses);
+
+    std::array<double, 3> man;
+    fillman(&man);
+    showman(man);
+
     return 0;
 }
 
@@ -37,4 +46,18 @@ void show(std::array<double, Seasons> da)
         total += da[i];
     }
     std::cout << "Total Expenses: $" << total << std::endl;
+}
+
+void fillman(std::array<double, 3>* pb) {
+    for (int i = 0; i < 3; i++) {
+        std::cout << "Enter " << triune[i] << ": ";
+        std::cin >> (*pb)[i];
+    }
+}
+
+void showman(std::array<double, 3> db) {
+    std::cout << "triune whatevs " << std::endl;
+    for (int i = 0; i < 3; i++) {
+        std::cout << triune[i] << " : " << db[i] << std::endl;
+    }
 }
