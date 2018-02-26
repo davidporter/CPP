9966fdb81696c6d2474cddc8f7567b85e9e04109
David Porter <davidbrianporter@gmail.com>
2018-02-25 20:07:46 -0500

    testing func addresses

--- funptr.cpp
+++ funptr.cpp
@@ -5,7 +5,9 @@ int func();
 int main()
 {
     int* func_ptr = (int*)(func);
+
     std::cout << "func()'s address is " << func_ptr << std::endl;
+    std::cout << "func " << func << std::endl; 
 }
 
 int func(){
