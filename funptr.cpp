bcf6b915551515c7f187cfc58141ccae2a8127ba
David Porter <davidbrianporter@gmail.com>
2018-02-25 21:04:18 -0500

    pointer to function program created and modified

--- funptr.cpp
+++ funptr.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
 
-int func();
+void func();
 
 int main()
 {
     int* func_ptr = (int*)(func);
 
+    std::cout << func() << std::endl;
     std::cout << "func()'s address is " << func_ptr << std::endl;
     std::cout << "func " << func << std::endl;
     std::cout << "&func " << func << std::endl; 
+
 }
 
-int func(){
+void func(){
     std::cout << "ima fun function";
 }
