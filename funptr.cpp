31a8b37d81803763d653e52119ece28144ab5a9a
David Porter <davidbrianporter@gmail.com>
2018-02-25 20:09:09 -0500

    func and &func equals 1 - does not make sense

--- funptr.cpp
+++ funptr.cpp
@@ -7,7 +7,8 @@ int main()
     int* func_ptr = (int*)(func);
 
     std::cout << "func()'s address is " << func_ptr << std::endl;
-    std::cout << "func " << func << std::endl; 
+    std::cout << "func " << func << std::endl;
+    std::cout << "&func " << func << std::endl; 
 }
 
 int func(){
